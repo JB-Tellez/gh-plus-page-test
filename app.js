7b9bc2c8d9071bbdf6932aa483e702c15fabd54f
JB Tellez <jb.tellez@gmail.com>
2018-01-13 22:37:10 -0800

    debug 22

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-page.base('/gh-plus-page-test')
+if (window.location !== '/') {
+    page.base('/gh-plus-page-test')
+}
+
 page('/', () => console.log('home'))
 page('/foo', () => console.log('foo'))
 page('/bar/baz', () => console.log('bar/baz'))
+
 page.start()
